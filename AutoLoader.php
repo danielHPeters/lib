16690cb049c1af6533408a0eb5240fb156cd6149
Daniel Peters <daniel.peters.ch@gmail.com>
2018-03-29 14:03:25 +0200

    Fix autoloader

--- AutoLoader.php
+++ AutoLoader.php
@@ -20,6 +20,7 @@ abstract class AutoLoader
             function ($class) {
                 $file = str_replace('\\', '/', $class);
                 $file = str_replace('rafisa/', '/', $file);
+                $file = str_replace('rio/', '../', $file);
                 $relativeUrl = dirname(__FILE__) . '/../' . $file . '.php';
                 if (file_exists($relativeUrl)) {
                     require_once $relativeUrl;
